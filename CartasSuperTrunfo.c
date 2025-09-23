8c386918a2717480669b4c1f8f666df210b219b9
Vergacas <jeanvergacas@gmail.com>
2025-09-23 12:19:06 -0300

    Criando variaveis

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -4,12 +4,16 @@
 // Tema 1 - Cadastro das cartas
 // Objetivo: No nível novato você deve criar as cartas representando as cidades utilizando scanf para entrada de dados e printf para exibir as informações.
 
-int main() {
+int main()
+{
   // Área para definição das variáveis para armazenar as propriedades das cidades
+  char estado, codigoCarta[3], cidade[20], estado2, codigoCarta2[3], cidade2[20];
+  int populacao, nPontosTuristicos, populacao2, nPontosTuristicos2;
+  float area, pib, area2, pib2;
 
   // Área para entrada de dados
 
   // Área para exibição dos dados da cidade
 
-return 0;
-} 
+  return 0;
+}
