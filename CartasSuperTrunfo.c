f8b55d06553c4f0421f67dc19e26eb935a3f61ba
Vergacas <jeanvergacas@gmail.com>
2025-09-28 09:23:59 -0300

    Criando a entrada de dados

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -27,6 +27,42 @@ int main()
   float pib2;
 
   // Área para entrada de dados
+  // Entrada de informções da Carta 1
+  printf("\n --Dados da Carta Numero um -- \n");
+  printf("Informe o Estado: ");
+  scanf("%c", &estado);
+  printf("Informe o Codigo da carta: ");
+  scanf("%3s", &codigoCarta);
+  while (getchar() != '\n'); //Limpar o buffer para envitar le uma linha vazia
+  printf("Informe o Nome da cidade: ");
+  fgets(cidade, sizeof(cidade), stdin);
+  printf("Informe a população da cidade: ");
+  scanf("%d", &populacao);
+  printf("Informe a Area da cidade(em km²): ");
+  scanf("%f", &area);
+  printf("Informe o PIB da cidade: ");
+  scanf("%f", &pib);
+  printf("Informe o numero de pontos turisticos: ");
+  scanf("%d", &nPontosTuristicos);
+
+  // Entrada de informções da Carta 2
+  printf("\n --Dados da Carta Numero Dois -- \n");
+  while (getchar() != '\n'); //Limpar o buffer para envitar le uma linha vazia
+  printf("Informe o Estado: ");
+  scanf("%c", &estado2);
+  printf("Informe o Codigo da carta: ");
+  scanf("%3s", &codigoCarta2);
+  while (getchar() != '\n'); //Limpar o buffer para envitar le uma linha vazia
+  printf("Informe o Nome da cidade: ");
+  fgets(cidade2, sizeof(cidade2), stdin);
+  printf("Informe a população da cidade: ");
+  scanf("%d", &populacao2);
+  printf("Informe a Area da cidade(em km²): ");
+  scanf("%f", &area2);
+  printf("Informe o PIB da cidade: ");
+  scanf("%f", &pib2);
+  printf("Informe o numero de pontos turisticos: ");
+  scanf("%d", &nPontosTuristicos2);
 
   // Área para exibição dos dados da cidade
 
